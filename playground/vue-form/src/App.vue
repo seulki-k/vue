7ca29bba2c711b3a373fa48f5db6146d03d900c0
Seulki Kang <skbb11@naver.com>
2024-12-23 16:54:53 +0900

    vue 기초 완

---
+++ playground/vue-form/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <!-- event.preventDefault(); 랑 같은 역할 -->
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id:</label>
+      <input
+        id="username"
+        type="text"
+        v-model="username"
+        placeholder="아이디"
+      />
+    </div>
+    <div>
+      <label for="password">password</label>
+      <input
+        id="passwrod"
+        type="password"
+        v-model="password"
+        placeholder="비번"
+      />
+    </div>
+    <button type="submt">login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function () {
+      // event.preventDefault(); // 새로고침 막는 방법
+      console.log(this.username, this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
